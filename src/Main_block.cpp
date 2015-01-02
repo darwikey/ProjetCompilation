7d8db9b35074ce9fde38a13d6db88b7d03057bbb
= <xavier@XavierDebian>
2015-01-02 15:24:53 +0100

    prise en charge des variables et tableaux globals

--- src/Main_block.cpp
+++ src/Main_block.cpp
@@ -1,7 +1,9 @@
 #include "Main_block.hpp"
 #include <stdexcept>
+#include <sstream>
 #include "Function.hpp"
 #include "Declarator.hpp"
+
 
 Main_block::Main_block(){
   // enregistrement des prototypes de printint() et printfloat()
@@ -26,7 +28,24 @@ std::string Main_block::get_code(std::vector<Block*> fParent_blocks, Function* f
 
   fParent_blocks.push_back(static_cast<Block*>(this));
   
-  // TODO variables globales
+  // variables globales
+  for (auto it : variables){
+    Declarator* var = it.second;
+
+    if (! var->is_function) {
+      int var_size = 4;
+      if (var->structure == Declarator_structure::ARRAY && var->type == Declarator_type::INT){
+	
+	var_size = 4 * var->array_size;
+      }
+
+      std::ostringstream str;
+      str << ".comm " << var->name << ", " << var_size << "\n";
+      code += str.str();
+    }
+  }
+
+  code += "\n";
   
   // code pour toutes les fonctions
   for (auto f : functions){
@@ -36,6 +55,70 @@ std::string Main_block::get_code(std::vector<Block*> fParent_blocks, Function* f
 
 
   return code;
+}
+
+
+std::string Main_block::get_code_load_variable(std::string fIdentifier, std::string fRegister){
+  auto it = variables.find(fIdentifier);
+
+  if (it != variables.end()){
+    std::ostringstream str;
+    str << "movl " << fIdentifier << ", %" << fRegister << "\n";
+    return str.str();
+  }
+
+  throw std::logic_error("global variable " + fIdentifier + " undeclared");
+  return "";
+}
+
+
+std::string Main_block::get_code_load_array(std::string fIdentifier, std::string fRegister_index, std::string fRegister_dest){
+  auto it = variables.find(fIdentifier);
+
+  if (it != variables.end()){
+    if (it->second->structure == Declarator_structure::ARRAY || it->second->structure == Declarator_structure::POINTER){
+      std::ostringstream str;
+
+      str << "movl "<< fIdentifier << "(, %" << fRegister_index << ", 4), %" << fRegister_dest << "\n";
+
+      return str.str();
+
+    }
+  }
+
+  throw std::logic_error(fIdentifier + " undeclared or is not an array or a pointer");
+  return "";
+}
+
+
+std::string Main_block::get_code_store_variable(std::string fIdentifier, std::string fRegister){
+  auto it = variables.find(fIdentifier);
+
+  if (it != variables.end()){
+    std::ostringstream str;
+    str << "movl %" << fRegister << ", " << fIdentifier << "\n";
+    return str.str();
+  }
+
+  throw std::logic_error("global variable " + fIdentifier + " undeclared");
+  return "";
+}
+
+
+std::string Main_block::get_code_store_array(std::string fIdentifier, std::string fRegister_index, std::string fRegister_dest){
+  auto it = variables.find(fIdentifier);
+
+  if (it != variables.end()){
+    if (it->second->structure == Declarator_structure::ARRAY || it->second->structure == Declarator_structure::POINTER){
+      std::ostringstream str;
+
+      str << "movl %" << fRegister_dest << ", " << fIdentifier << "(, %" << fRegister_index << ", 4)\n";
+      return str.str();
+    }
+  }
+
+  throw std::logic_error(fIdentifier + " undeclared or is not an array or a pointer");
+  return "";
 }
 
 
